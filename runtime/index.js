25fac28f77c762aeec298eacc7f036d8b4297555
WobbyChip <66483092+WobbyChip@users.noreply.github.com>
2022-06-11 01:22:58 +0300

    1

--- runtime/index.js
+++ runtime/index.js
@@ -2,5 +2,5 @@ const http = require("http");
 const express = require("express");
 
 const app = express();
-app.use(express.static("./../", {index: "index.html"}));
+app.use(express.static("./../", { index: "index.html" }));
 http.createServer(app).listen(80);
